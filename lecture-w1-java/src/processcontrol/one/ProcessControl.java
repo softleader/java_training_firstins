2186cb70d5f6671729142cac454781adef3ffa80
pighouse7647 <pighouse7647@gmail.com>
2018-08-14 23:55:30 +0800

    1.adj rename package. 2.adj hidden answer.

---
+++ lecture-w1-java/src/processcontrol/one/ProcessControl.java
@@ -0,0 +1,139 @@
+package processcontrol.one;
+
+import com.google.inject.internal.util.Maps;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+public class ProcessControl {
+
+  enum SwitchType {
+    A,
+    B,
+    C,
+  }
+
+  public static void main(String[] args) {
+//		method1();
+//		method2((int) (Math.random()*10), (int) (Math.random()*10));
+//		method3();
+//		method4();
+//		method5();
+//		method6();
+//    method7();
+//    method8();
+
+  }
+
+  // if...else if...else
+  private static void method1() {
+    int b = 10;
+    int a = 11;
+    // TODO if ... else if...else
+      System.out.println(a + " > " + b);
+      System.out.println(a + " < " + b);
+      System.out.println(a + " = " + b);
+  }
+
+  // extract local variable to parameter
+  private static void method2(int a, int b) {
+    // TODO extract local variable to parameter
+
+  }
+  // while
+  private static void method3() {
+    int i = 0;
+    while (i < 100) {
+      System.out.printf("%3d: ", i + 1); // align at 3 digits
+      // TODO call method2
+      i++;
+    }
+  }
+  // do...while...
+  private static void method4() {
+    int i = 0;
+    do {
+      // TODO
+    } while (i < 100);
+  }
+  // 9X9(vertical)
+  private static void method5() {
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        System.out.printf("%d * %d = %d \n", i + 1, j + 1, (i + 1) * (j + 1));
+      }
+    }
+  }
+  // 9X9 (horizontal)
+  private static void method6() {
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        // TODO
+        // TODO and make it pretty...
+      }
+      // Hint: System.out.printf("\n");
+    }
+  }
+  // Map & switch
+  private static void method7() {
+    LinkedHashMap<SwitchType, Object> m = Maps.newLinkedHashMap();
+    m.put(SwitchType.A, "Tom");
+    m.put(SwitchType.B, "Jane");
+    m.put(SwitchType.C, "Meta");
+    m.put(SwitchType.A, "Joy");
+    m.put(SwitchType.B, "Matt");
+    m.put(SwitchType.C, "Rhys");
+    m.put(SwitchType.A, "David");
+    m.put(SwitchType.B, "Tony");
+    m.put(SwitchType.C, "Sam");
+
+    // To figured out why there are only 3 results?
+    m.forEach((k, v) -> {
+      switchPrinter(k, (String) v);
+    });
+  }
+  // Entry & switch
+  private static void method8() {
+    Entry[] entries = {
+        new SimpleEntry(SwitchType.A, "Tom"),
+        new SimpleEntry(SwitchType.B, "Jane"),
+        new SimpleEntry(SwitchType.C, "Meta"),
+        new SimpleEntry(SwitchType.A, "Joy"),
+        new SimpleEntry(SwitchType.B, "Matt"),
+        new SimpleEntry(SwitchType.C, "Rhys"),
+        new SimpleEntry(SwitchType.A, "David"),
+        new SimpleEntry(SwitchType.B, "Tony"),
+        new SimpleEntry(SwitchType.C, "Sam")
+    };
+
+    for (Entry<SwitchType, String> entry : entries) {
+      SwitchType k = null; // TODO
+      String v = ""; // TODO
+      switchPrinter(k, v);
+    }
+  }
+
+
+  private static void switchPrinter(SwitchType k, String v) {
+    switch (k) {
+      case A:
+        System.out.printf("Team %s \t %s\n", k.name(), v);
+        break;
+      case B:
+        System.out.printf("Team %s \t %s\n", k.name(), v);
+        break;
+      case C:
+        System.out.printf("Team %s \t %s\n", k.name(), v);
+        break;
+      default:
+        System.out.printf("Team %s \t %s\n", k.name(), v);
+        break;
+
+    }
+  }
+
+
+
+
+
+}
--- lecture-w1-java/src/processcontrol/two/ProcessControl.java
+++ lecture-w1-java/src/processcontrol/two/ProcessControl.java
@@ -1,6 +1,9 @@
-package processControlTwo;
+package processcontrol.two;
 
+import com.google.inject.internal.util.Maps;
 import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,54 +23,54 @@ public class ProcessControl {
   class Person {
 
     public String getName() {
-		return name;
-	}
+      return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+      this.name = name;
+    }
 
-	public Integer getEmpNo() {
-		return empNo;
-	}
+    public Integer getEmpNo() {
+      return empNo;
+    }
 
-	public void setEmpNo(Integer empNo) {
-		this.empNo = empNo;
-	}
+    public void setEmpNo(Integer empNo) {
+      this.empNo = empNo;
+    }
 
-	public Gender getGender() {
-		return gender;
-	}
+    public Gender getGender() {
+      return gender;
+    }
 
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
+    public void setGender(Gender gender) {
+      this.gender = gender;
+    }
 
-	public BigDecimal getWageRate() {
-		return wageRate;
-	}
+    public BigDecimal getWageRate() {
+      return wageRate;
+    }
 
-	public void setWageRate(BigDecimal wageRate) {
-		this.wageRate = wageRate;
-	}
+    public void setWageRate(BigDecimal wageRate) {
+      this.wageRate = wageRate;
+    }
 
-	public BigDecimal getWorkHour() {
-		return workHour;
-	}
+    public BigDecimal getWorkHour() {
+      return workHour;
+    }
 
-	public void setWorkHour(BigDecimal workHour) {
-		this.workHour = workHour;
-	}
+    public void setWorkHour(BigDecimal workHour) {
+      this.workHour = workHour;
+    }
 
-	public BigDecimal getSalary() {
-		return salary;
-	}
+    public BigDecimal getSalary() {
+      return salary;
+    }
 
-	public void setSalary(BigDecimal salary) {
-		this.salary = salary;
-	}
+    public void setSalary(BigDecimal salary) {
+      this.salary = salary;
+    }
 
-	private String name;
+    private String name;
     private Integer empNo;
     private Gender gender;
     private BigDecimal wageRate;
@@ -85,47 +88,47 @@ public class ProcessControl {
   class PersonBuilder {
 
     public String getName() {
-		return name;
-	}
+      return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+      this.name = name;
+    }
 
-	public Integer getEmpNo() {
-		return empNo;
-	}
+    public Integer getEmpNo() {
+      return empNo;
+    }
 
-	public void setEmpNo(Integer empNo) {
-		this.empNo = empNo;
-	}
+    public void setEmpNo(Integer empNo) {
+      this.empNo = empNo;
+    }
 
-	public Gender getGender() {
-		return gender;
-	}
+    public Gender getGender() {
+      return gender;
+    }
 
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
+    public void setGender(Gender gender) {
+      this.gender = gender;
+    }
 
-	public BigDecimal getWageRate() {
-		return wageRate;
-	}
+    public BigDecimal getWageRate() {
+      return wageRate;
+    }
 
-	public void setWageRate(BigDecimal wageRate) {
-		this.wageRate = wageRate;
-	}
+    public void setWageRate(BigDecimal wageRate) {
+      this.wageRate = wageRate;
+    }
 
-	public BigDecimal getWorkHour() {
-		return workHour;
-	}
+    public BigDecimal getWorkHour() {
+      return workHour;
+    }
 
-	public void setWorkHour(BigDecimal workHour) {
-		this.workHour = workHour;
-	}
+    public void setWorkHour(BigDecimal workHour) {
+      this.workHour = workHour;
+    }
 
 
-	private String name;
+    private String name;
     private Integer empNo;
     private Gender gender;
     private BigDecimal wageRate;
@@ -171,35 +174,36 @@ public class ProcessControl {
 
 
   public static void main(String[] args) {
-//    method1();
+    method1();
 //    method2();
 //    method3();
 //    method4();
 
   }
+
   // forEach
   private static void method1() {
-    Person p1 = new PersonBuilder().name("Matt").empNo(1).gender(Gender.MALE).build();
-    Person p2 = new PersonBuilder().name("David").empNo(2).gender(Gender.MALE).build();
-    Person p3 = new PersonBuilder().name("Rhys").empNo(3).gender(Gender.MALE).build();
-    Person p4 = new PersonBuilder().name("Thomas").empNo(4).gender(Gender.MALE).build();
-    Person p5 = new PersonBuilder().name("Meta").empNo(5).gender(Gender.FEMALE).build();
-    Person p6 = new PersonBuilder().name("Sophie").empNo(6).gender(Gender.FEMALE).build();
-    Person p7 = new PersonBuilder().name("Sunny").empNo(7).gender(Gender.FEMALE).build();
-    Person p8 = new PersonBuilder().name("Cathy").empNo(8).gender(Gender.FEMALE).build();
-
-    Map<Gender, List<Person>> empls = getEmpls()
-        .stream().collect(Collectors.groupingBy(Person::getGender, Collectors.toList()));
+    Map<String, List<Person>> empls =
+//        Maps.newLinkedHashMap(); // TODO group by Name
+        getEmpls().stream().collect(Collectors.groupingBy(Person::getName));
+    // TODO sorted by empNo
+    empls = empls.entrySet().stream()
+//        .sorted(Comparator.comparing((entry) -> entry.getValue().stream().map(Person::getEmpNo).min(Comparator.reverseOrder()).get()))
+        .collect(LinkedHashMap::new, (m, entry) -> m.put(entry.getKey(), entry.getValue()), Map::putAll)
+;
 
     // Map
-    empls.forEach((gender, emps) -> {
-      System.out.printf("\n\nMember in gender %s:\n", gender.name());
+    empls.forEach((name, emps) -> {
+      System.out.printf("\n\n%s:\n", name);
       // Object
       emps.forEach(emp -> {
-        System.out.printf("%s(%03d)\t", emp.getName(), emp.getEmpNo());
+        System.out.printf("(%03d)\t", emp.getEmpNo());
       });
     });
+
+
   }
+
   // Grouping by gender
   private static void method2() {
     Map<Gender, List<Person>> empls = getEmpls().stream()
@@ -213,6 +217,7 @@ public class ProcessControl {
       });
     });
   }
+
   // Grouping by work hour.
   private static void method3() {
 
@@ -230,25 +235,20 @@ public class ProcessControl {
       });
     });
   }
+
   // filter
   private static void method4() {
     List<Person> tiredEmpls = getEmpls().stream()
         .filter(p -> p.getWorkHour().compareTo(BigDecimal.valueOf(8)) > 0)
         .collect(Collectors.toList());
 
-
     tiredEmpls.forEach(emp -> {
 
-      System.out.printf("%s\t(%03d)\t(%s)\t\n", emp.getName(), emp.getEmpNo(), emp.getWorkHour().toPlainString());
-
-
-
+      System.out.printf("%s\t(%03d)\t(%s)\t\n", emp.getName(), emp.getEmpNo(),
+          emp.getWorkHour().toPlainString());
 
 
     });
-
-
-
 
 
   }
@@ -280,7 +280,6 @@ public class ProcessControl {
         .build();
     return Stream.of(p1, p2, p3, p4, p5, p6, p7, p8).collect(Collectors.toList());
   }
-
 
 
 }
