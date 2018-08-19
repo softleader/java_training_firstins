7135b66ab050931367efde76be3af47241ad3352
pighouse7647 <pighouse7647@gmail.com>
2018-08-19 22:38:54 +0800

    1.remove redundant files

--- lecture-w2-servlet/src/lambda/customized/CustomizedLambdaExecutor.java
+++ lecture-w2-servlet/src/lambda/customized/CustomizedLambdaExecutor.java
@@ -7,13 +7,20 @@ import lambda.customized.type.streamlike.impl.CustomizedStreamLikeTypeImpl;
 public class CustomizedLambdaExecutor {
 
 	public static void main(String[] args) {
-		
+		CustomizedF1<Integer, Boolean> condition = a -> a > 1;
+		CustomizedF1<Integer, String> mapper = a -> "號碼[" + a + "]";
+
+
 		// 即使改成static import, 仍依舊像是函數呼叫, 而非管線操作.
 		System.out.println(CustomizedHelperLikeType.cons(1, CustomizedHelperLikeType.nil()));
 		System.out.println(CustomizedHelperLikeType.cons(1, CustomizedHelperLikeType.cons(2, CustomizedHelperLikeType.nil())));		
 		System.out.println(CustomizedHelperLikeType.cons(1, CustomizedHelperLikeType.cons(2, CustomizedHelperLikeType.cons(3, CustomizedHelperLikeType.nil()))));
-		
-		
+
+		CustomizedHelperLikeType.map(
+				CustomizedHelperLikeType.filter(
+						CustomizedHelperLikeType.list(1, 2, 3), condition), mapper);
+
+
 		// Java8 default method 預設方法
 		System.out.println(new CustomizedStreamLikeTypeImpl().cons(1, new CustomizedStreamLikeTypeImpl().nil()));
 		System.out.println(new CustomizedStreamLikeTypeImpl().cons(1, new CustomizedStreamLikeTypeImpl().cons(2, new CustomizedStreamLikeTypeImpl().nil())));
@@ -24,8 +31,7 @@ public class CustomizedLambdaExecutor {
 			// 再將tail設定為
 				// 呼叫cons時先將3設定為head, 
 				// 再將tail設定為Nil(head為null; tail為null; toString()為[]）
-		CustomizedF1<Integer, Boolean> condition = a -> a > 1;
-		CustomizedF1<Integer, String> mapper = a -> "號碼[" + a + "]";
+
 		
 		System.out.println(CustomizedStreamLikeTypeImpl.of(1, 2, 3)
 				.filter(condition)
