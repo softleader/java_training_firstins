7856a02f8102eca53e4a58c87100e66344e19b0f
sevenlfanks <godjdc@gmail.com>
2018-09-04 12:29:18 +0800

    update

--- lecture-w4-spring-data/src/main/java/com/example/firstins/common/AbstractEntity.java
+++ lecture-w4-spring-data/src/main/java/com/example/firstins/common/AbstractEntity.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
@@ -13,7 +14,7 @@ import javax.persistence.PreUpdate;
 public abstract class AbstractEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
 
 	@Column
--- lecture-w4-spring-data/src/test/java/com/example/firstins/LectureW4SpringDataApplicationTests.java
+++ lecture-w4-spring-data/src/test/java/com/example/firstins/LectureW4SpringDataApplicationTests.java
@@ -1,78 +1,11 @@
 package com.example.firstins;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.example.firstins.member.Member;
-import com.example.firstins.member.MemberDao;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LectureW4SpringDataApplicationTests {
-
-	@Value("${spring.datasource.driver-class-name}")
-	private String dsJdbcDriver;
-
-	@Value("${spring.datasource.url}")
-	private String dsJdbcUrl;
-
-	@Value("${spring.datasource.username}")
-	private String dsUsername;
-
-	@Value("${spring.datasource.password}")
-	private String dsPassword;
-
-	@Autowired
-	private MemberDao memberDao;
-
-	@Test
-	public void jpaExample() {
-		final Member member = new Member();
-		final String name = "Rhys";
-		member.setName(name);
-		memberDao.save(member);
-		memberDao.findAll().forEach(m ->
-				System.out.println(ToStringBuilder.reflectionToString(m, ToStringStyle.SHORT_PREFIX_STYLE)));
-
-		memberDao.findByName(name).forEach(m ->
-				System.out.println(ToStringBuilder.reflectionToString(m, ToStringStyle.SHORT_PREFIX_STYLE)));
-	}
-
-	@Test
-	public void jdbcExample() throws Exception {
-		Class.forName(dsJdbcDriver);
-		final Connection connection = DriverManager.getConnection(dsJdbcUrl, dsUsername, dsPassword);
-
-		// Insert
-		final PreparedStatement preparedStatement1 = connection
-				.prepareStatement("INSERT INTO MEMBER (ID, NAME) VALUES (?, ?)");
-		preparedStatement1.setLong(1, 1L);
-		preparedStatement1.setString(2, "Rhys");
-		final int inserted = preparedStatement1.executeUpdate();
-		System.out.println("inserted row " + inserted);
-
-		// Query
-		final PreparedStatement preparedStatement2 = connection
-				.prepareStatement("SELECT * FROM MEMBER");
-		final ResultSet resultSet = preparedStatement2.executeQuery();
-		if (resultSet.next()) {
-			final Member member = new Member();
-			member.setId(resultSet.getLong("id"));
-			member.setName(resultSet.getString("name"));
-			System.out.println(ToStringBuilder.reflectionToString(member, ToStringStyle.SHORT_PREFIX_STYLE));
-		}
-
-	}
 
 }
