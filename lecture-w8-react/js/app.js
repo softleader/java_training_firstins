ecc105b9532c70d472ddba0692e0d3e841a11445
David Hsu <david.hsu.sl@gmail.com>
2018-10-04 10:41:55 +0800

    update

--- lecture-w8-react/js/app.js
+++ lecture-w8-react/js/app.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MemberList from "./containers/MemberList";
+import thunkMiddleware from 'redux-thunk';
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import reducers from "./reducers/reducers";
+import {createLogger} from "redux-logger";
+import {Provider} from "react-redux";
+
+const store = createStore(
+  combineReducers({
+    ...reducers,
+  }),
+  applyMiddleware(
+    thunkMiddleware,
+    createLogger())
+);
 
 window.app = {};
 
@@ -9,5 +23,10 @@ app.create = (dom) => {
 };
 
 app.run = (dom) => {
-  ReactDOM.render(<MemberList/>, dom)
+  ReactDOM.render(
+    <Provider store={store}>
+      <MemberList/>
+    </Provider>,
+    dom
+  )
 };
